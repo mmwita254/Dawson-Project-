bee608d3b07674b8b390fdb2899d2af1b0703bb3
Nelson Muthiga <nelsonmuthiga@gmail.com>
2024-10-22 21:34:31 +0000

    create project interface and refactor some of the types

--- frontend/src/common/types.ts
+++ frontend/src/common/types.ts
@@ -1,12 +1,12 @@
 export interface Document {
   documentId: string; // Use camelCase for consistency
+  projectId: string; //link the file to a specific project
   userId: string;     // Use camelCase for consistency
   fileName: string;   // Use camelCase for consistency
   fileSize: number;   // Change to number if it's a numeric value
   docStatus: string;  // Use camelCase for consistency
   created: string;    // Consider using Date type if it’s a date
   pages: number;      // Change to number if it's a numeric value
-  conversations: Conversation[]; // Adjusted type reference
 }
 
 export interface Conversation {
@@ -22,4 +22,15 @@ export interface Message {
     example: boolean;
     additionalKwargs: Record<string, any>; // More generic for additional kwargs
   };
+}
+
+
+export interface Project {
+  projectId: string;          // Unique identifier for the project
+  projectName: string;        // Name of the project
+  userId: string;             // User who owns or created the project
+  created: string;            // Date when the project was created (Consider using Date type)
+  description?: string;       // Optional description of the project
+  documents: Document | Document[];      // List of documents associated with this project
+  conversations: Conversation | Conversation[]
 }
